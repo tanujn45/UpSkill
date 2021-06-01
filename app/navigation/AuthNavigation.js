f7811215c5825d10e694b588e4d8c140bf327a98
tanujn45 <tanujn45@gmail.com>
2021-06-01 17:52:19 +0530

    changes

--- app/navigation/AuthNavigation.js
+++ app/navigation/AuthNavigation.js
@@ -3,13 +3,23 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SignIn from '../screens/SignIn';
+import HomeScreen from '../screens/HomeScreen';
 
 const Stack = createStackNavigator();
 
 const AuthNavigation = () => {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Welcome" component={SignIn} />
+            <Stack.Screen
+                options={{ headerShown: false }}
+                name="Welcome"
+                component={SignIn}
+            />
+            <Stack.Screen
+                options={{ headerShown: false }}
+                name="SomeScreen"
+                component={HomeScreen}
+            />
         </Stack.Navigator>
     );
 };
